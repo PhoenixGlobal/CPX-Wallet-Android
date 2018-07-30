d6f8aa8c2b54ae16e6cd77ac3f51acff2c1733d6
Ailsa <liuyi_61@163.com>
2018-07-30 13:20:17 +0800

    v1.0.0

--- app/src/main/java/chinapex/com/wallet/executor/callback/IGetUtxosCallback.java
+++
@@ -1,9 +0,0 @@
-package chinapex.com.wallet.executor.callback;
-
-/**
- * Created by SteelCabbage on 2018/6/8 00:08
- * E-Mail：liuyi_61@163.com
- */
-public interface IGetUtxosCallback {
-    void getUtxos(String utxos);
-}
